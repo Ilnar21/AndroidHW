f65e60385dbaab06ccc3bc81738f75667fdd3b5e
Ilnar21 <valiullinilnar2005@gmail.com>
2024-07-01 09:37:32 +0300

    my new hw1

---
+++ hw_1/app/src/main/java/RaceManager.kt
@@ -0,0 +1,45 @@
+package com.example.hw_1
+
+import android.util.Log
+
+class RaceManager {
+
+    companion object {
+        const val TAG = "RaceManager"
+    }
+
+    private fun race(car1: Car, car2: Car): Car {
+        Log.d(TAG, "The race between ${car1.brand} ${car1.model} and ${car2.brand} ${car2.model}")
+
+        return if (car1.horsepower > car2.horsepower) {
+            Log.d(TAG, "Winner: ${car1.brand} ${car1.model}")
+            car1
+        } else {
+            Log.d(TAG, "Winner: ${car2.brand} ${car2.model}")
+            car2
+        }
+    }
+
+    fun organizeRaces(cars: List<Car>): Car {
+        var remainingCars = cars.toMutableList()
+
+        while (remainingCars.size > 1) {
+            val nextRound = mutableListOf<Car>()
+            val shuffledCars = remainingCars.shuffled()
+
+            for (i in 0 until shuffledCars.size step 2) {
+                if (i + 1 < shuffledCars.size) {
+                    val winner = race(shuffledCars[i], shuffledCars[i + 1])
+                    nextRound.add(winner)
+                } else {
+                    Log.d(TAG,
+                        "${shuffledCars[i].brand} ${shuffledCars[i].model} - No pair, next round")
+                    nextRound.add(shuffledCars[i])
+                }
+            }
+            remainingCars = nextRound
+        }
+
+        return remainingCars.first()
+    }
+}
